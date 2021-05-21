853041d0806c910d6f417d8ae2ed6d535adfcad1
Flowsqy <Flo504.Dev@gmail.com>
2021-05-21 11:22:33 +0200

    Add input block and input check

--- js/script.js
+++ js/script.js
@@ -8,14 +8,11 @@ tab=[
     "\n   +-------+\n   |       |\n   |       O\n   |      -|-\n   |      | |\n   |\n=============="
     ]
 
-window.addEventListener("load", init);
+window.addEventListener("load", initWord);
 
 var word;
 var devine;
-
-function init(){
-    initWord();
-}
+var inputField;
 
 function initWord(){
     document.getElementById("file-input").addEventListener("change", function(){
@@ -25,7 +22,59 @@ function initWord(){
         reader.addEventListener("load", function(){
             let words = this.result.split("\n");
             word = words[Math.floor(Math.random() * words.length)];
+            createInputDiv();
+            //game();
         })
         reader.readAsText(file);
     })
+}
+
+function createInputDiv(){
+    let mainContainer = document.getElementById("main-container");
+    inputField = document.createElement("input");
+    let inputDiv = document.createElement("div");
+    inputDiv.className = "center";
+    inputField.type = "text";
+    inputField.addEventListener("change", loop);
+    let validbutton = document.createElement("button");
+    validbutton.textContent = "Valider";
+    validbutton.addEventListener("click", loop);
+    inputDiv.appendChild(inputField);
+    inputDiv.appendChild(validbutton);
+    mainContainer.appendChild(inputDiv);
+}
+
+function askLetter(){
+    return inputField.value;
+}
+
+function displayInfo(info){
+    document.getElementById("info").innerHTML = info;
+}
+
+/*
+function game(){
+    while(!loop()){}
+}*/
+
+function loop(){
+    let proposition = askLetter();
+    let letter = checkLetter(proposition);
+    if(letter == null){
+
+        return false;
+    }
+    return true;
+}
+
+function checkLetter(proposition){
+    if(proposition.length != 1){
+        return null;
+    }
+    let character = proposition.charAt(0);
+    var letters = /^[A-Za-z]+$/;
+    if(!character.match(letters)){
+        return null;
+    }
+    return character.toUpperCase();
 }
